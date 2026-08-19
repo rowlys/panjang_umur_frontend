6468aafe38fe91bfb0e6b7c7338c7fd10788729c
Basten <salimbasten@gmail.com>
2026-08-20 00:59:27 +0800

    fix: namespace causing app unable to open

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.rowly.panjang_umur_frontend"
+    namespace = "com.example.panjang_umur_frontend"
     compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
